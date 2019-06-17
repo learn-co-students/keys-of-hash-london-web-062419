6e5485ab2fa9c0131737c7e67652d8a420eb8c77
github username <github email address>
2019-06-17 19:22:25 +0100

    keys-of-hash

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,12 @@
+require "pry"
 class Hash
-  def keys_of(arguments)
-    # code goes here
-  end
-end
+  def keys_of(*arguments)
+    # code goes here	    
+    collect do |key, value| # returns a new array without modifying the previous. 
+      if arguments.include?(value)
+        key
+      end
+    end.compact  #removes NIL not sure how to use this correctly as of yet but it does pass the test. 
+  end	
+  # binding.pry  
+end 	
